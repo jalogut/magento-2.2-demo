a69409231bd289774f4adc79b1e651bf13f81b19
ilyassmoutite <45095556+ilyassmoutite@users.noreply.github.com>
2021-05-17 18:54:20 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,34 +6,39 @@
 # -x display commands for debugging
 set -eux
 
-GIT_REPO=https://github.com/jalogut/magento-2.2-demo.git
-LANGUAGES='en_US de_CH'
-STATIC_DEPLOY_PARAMS='--exclude-theme=Magento/blank'
-DOWNLOADS_DIR='downloads'
+# ============================
+# NOTES:
+# This file goes into Jenkins /usr/local/bin
+# Properties (LANGUAGES, STATIC_DEPLOY_PARAMS, DISABLE_MODULES) are set in the project:
+#	 - https://github.com/jalogut/magento-2.2-demo/blob/master/config/properties.sh
+# TAR excludes also defined in project:
+#    - https://github.com/jalogut/magento-2.2-demo/blob/master/config/artifact.excludes
+# ============================
+source ${WORKING_DIR}/config/properties.sh
+ARTIFACT_EXCLUDES=${WORKING_DIR}/config/artifact.excludes
 
 WORKING_DIR=`pwd`
 MAGENTO_DIR='magento'
-VERSION='develop'
-BUILD=${WORKING_DIR}/${VERSION}
+
+BUILD=${WORKING_DIR}
 
 # GET CODE
-git clone --depth 1 --branch ${VERSION} ${GIT_REPO} ${BUILD}
+# git clone not needed for Jenkins
 cd ${BUILD}
 composer install --no-dev --prefer-dist --optimize-autoloader
 
 # GENERATE FILES
 cd ${BUILD}/${MAGENTO_DIR}
+if [[ -n ${DISABLE_MODULES} ]]; then
+    bin/magento module:disable ${DISABLE_MODULES}
+fi
 bin/magento setup:di:compile
 bin/magento setup:static-content:deploy -f ${LANGUAGES} ${STATIC_DEPLOY_PARAMS}
 find var vendor pub/static pub/media app/etc -type f -exec chmod g+w {} \; && find var vendor pub/static pub/media app/etc -type d -exec chmod g+w {} \;
 
 # CREATE ARTIFACT
 cd ${BUILD}
-ARTIFACT_FILENAME=../${VERSION}.tar.gz
-tar --exclude-from=/home/ilyass/artifact.excludes -czf ${ARTIFACT_FILENAME} .
-
-# TRANSFER ARTIFACT
-#scp -P 22 ${ARTIFACT_FILENAME} ${SERVER_USERNAME}@${SERVER_HOST}:${DOWNLOADS_DIR}
+tar --exclude-from=${ARTIFACT_EXCLUDES} -czf ${ARTIFACT_FILENAME} .
 
 # RETURN TO WORKING DIR
 cd ${WORKING_DIR}
